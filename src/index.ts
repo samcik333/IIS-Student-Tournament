825363905e16043249669b670472937f01a4d65d
Samuel Sadlek <samcik333@gmail.com>
2022-10-10 02:30:16 +0200

    change index

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import express from "express"
-const app = express()
-const port = process.env.PORT || 3000
+import path from "path"
+const PORT = process.env.PORT || 5000
 
-app.get("/", (req: any, res: any) => {
-  res.send("Hello World!")
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+express()
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", (req, res) => res.render("pages/index"))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
