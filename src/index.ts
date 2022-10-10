29df3e325ffcaef07ab39d6b5fab8419ccac3537
Samuel Sadlek <samcik333@gmail.com>
2022-10-10 02:33:22 +0200

    change index

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,5 @@ import path from "path"
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, "public")))
-  .set("views", path.join(__dirname, "views"))
-  .set("view engine", "ejs")
-  .get("/", (req, res) => res.render("pages/index"))
+  .get("/", (req, res) => res.send("HELLO"))
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
