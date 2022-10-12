7b4e74135216a84ff16eee6f1a15ad4517c3a226
Samuel Sadlek <samcik333@gmail.com>
2022-10-12 18:34:55 +0200

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import express from "express"
-import path from "path"
 const PORT = process.env.PORT || 5000
 
 express()
   .get("/", (req, res) => res.send("HELLO"))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+  .listen(PORT, () => console.log(`Listening on localhost${PORT}`))
