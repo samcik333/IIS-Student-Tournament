9edb6ea6fceb596672d8bd8007bee5b96218e645
Samuel Sadlek <samcik333@gmail.com>
2022-10-16 19:55:21 +0200

    fix router

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # IIS-Student-Tournament
 
-This is text for test
-This is text for test2
+Ako prvé si stiahnite tieto komponenty:  
+node.js, npm, node
+
+Po naklonovaní repozitára si ako prve zadajte `make` alebo `make init`  
+Aby ste využívali lokalné prostredie zadajte `make up`
+
+Pokiaľ by ste mali nejaké problémy tak mi napíšte
--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,10 @@
 import express from "express"
-const PORT = process.env.PORT || 5000
 
-express()
-  .get("/", (req, res) => res.send("SJS Squad"))
-  .listen(PORT, () => console.log(`Listening on localhost${PORT}`))
+const PORT = process.env.PORT || 5000
+const router = express()
+
+router.listen(PORT, () =>
+  console.log(`Listening on localhost ${PORT}`)
+)
+
+export default router
