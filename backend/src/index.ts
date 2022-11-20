224cd11ccf5163a2ec205f113bc7e6ec1a863519
Samueel12 <samuel.sulo@gmail.com>
2022-11-20 22:59:46 +0100

    User UI

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,12 +8,11 @@ import routes from "./routes/index";
 import bodyParser from "body-parser";
 import cors from "cors";
 
-
 const knex = Knex(knexConfiig.development);
 
 Model.knex(knex);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5005;
 const app = express();
 
 app.use(cors());
@@ -24,5 +23,5 @@ app.use(bodyParser.json());
 app.use("/", routes);
 
 app.listen(PORT, () => {
-  console.log(`app running on port ${PORT}`);
+	console.log(`app running on port ${PORT}`);
 });
