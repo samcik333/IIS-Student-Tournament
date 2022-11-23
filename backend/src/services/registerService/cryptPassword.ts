7a9d9312a5239eaae6d2e65a8888f906b8be044b
Samuel Sadlek <samcik333@gmail.com>
2022-11-23 13:54:23 +0100

    refactory

--- backend/src/services/registerService/cryptPassword.ts
+++
@@ -1,5 +0,0 @@
-import bcrypt from "bcrypt";
-
-export default async function encryptPassword(password: string) {
-  return await bcrypt.hash(password, 10);
-}
--- backend/src/services/tournamentService/find.ts
+++
@@ -1,5 +0,0 @@
-import Tournament from "../../models/tournamentModel";
-
-export const findTournament = async (id: string) => {
-    return await Tournament.query().findOne("id", id);
-}
