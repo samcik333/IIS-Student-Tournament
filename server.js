39277de9078da62d922a77b67f48fcbe1d6d1307
Samuel Sadlek <samcik333@gmail.com>
2022-11-23 14:48:23 +0100

    my heroku angular app

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/demopr'));
+app.use(express.static(__dirname + '/dist/frontend'));
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/demopr/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/frontend/index.html'));
 });
 app.listen(process.env['PORT'] || 5005);
