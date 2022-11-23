97acab7a935de2849ddeec7596b0d0098f26b44d
Samuel Sadlek <samcik333@gmail.com>
2022-11-23 02:07:52 +0100

    make it better

--- backend/knexfile.ts
+++ backend/knexfile.ts
@@ -17,7 +17,17 @@ export = {
   },
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
+    connection: {
+      database: process.env.DATABASE_NAME || "d4mvrd2rol6mkb",
+      user: process.env.POSTGRES_USER || "klazldwbxlxrra",
+      password:
+        process.env.POSTGRES_PASSWORD ||
+        "52b8ebebbd910de17b0855c84e685952aa0e1048e3f79880659b6d16d2f7de39",
+      port: process.env.POSTGRES_PORT || 5432,
+      host:
+        process.env.POSTGRES_HOST ||
+        "ec2-63-32-248-14.eu-west-1.compute.amazonaws.com",
+    },
     migrations: {
       directory: "./src/migrations",
     },
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,14 +14,17 @@ Model.knex(knex);
 
 const PORT = process.env.PORT || 5005;
 const app = express();
+var distDir = __dirname + "/dist/";
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.json());
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(distDir));
 app.use("/", routes);
 
 app.listen(PORT, () => {
-	console.log(`app running on port ${PORT}`);
+  console.log(`app running on port ${PORT}`);
 });
