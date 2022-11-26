0e1946ba2736a5c0f580032f0a991e6633258988
xxGEORGEx <jurajbagi@hotmail.sk>
2022-11-26 20:07:53 +0100

    tur-info/players

--- src/app/tournament/players/players.component.ts
+++ src/app/tournament/players/players.component.ts
@@ -13,15 +13,23 @@ import { TournamentService } from 'src/app/shared/tournament.service';
 export class PlayersComponent implements OnInit {
 
   myParam!: string;
-  users!: User;
-  teams!: Team;
+  users!: User[];
+  teams!: Team[];
+  num: number = 0;
   tournament!: Tournament;
   constructor(private route: ActivatedRoute, private routerTournament:TournamentService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => this.myParam = params['id']);
+    this.routerTournament.find(this.myParam).subscribe((response:Tournament) => {
+      this.tournament = response;
+    });;
     this.routerTournament.getParticipants(this.myParam).subscribe((response:any) => {
-      console.log(response);
+        if(response.result && response.result.type == "teams"){
+          this.teams = response.result.teams;
+        }else if(response.result && response.result.type == "users"){
+          this.users = response.result.users;
+        }
     });
   }
 
