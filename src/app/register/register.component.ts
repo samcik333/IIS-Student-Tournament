557f5587db502721763c78fde48ca58e1f9b2206
Samuel Sadlek <samcik333@gmail.com>
2022-11-27 01:58:21 +0100

    login and register update

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,21 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import {Component, OnInit} from "@angular/core";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {MatDialog} from "@angular/material/dialog";
+import {switchMap} from "rxjs";
+import {User} from "../model/user";
+import {LoginService} from "../shared/login.service";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css'],
+	selector: "app-register",
+	templateUrl: "./register.component.html",
+	styleUrls: ["./register.component.css"],
 })
 export class RegisterComponent implements OnInit {
-  constructor() {}
+	isUserExist = false;
+	constructor(
+		private loginService: LoginService,
+		private dialog: MatDialog
+	) {}
 
-  ngOnInit(): void {}
+	ngOnInit(): void {}
 
-  registerGroup = new FormGroup({
-    username: new FormControl(),
-    password: new FormControl(),
-    email: new FormControl(),
-    name: new FormControl(),
-    surname: new FormControl(),
-  });
+	registerGroup = new FormGroup({
+		username: new FormControl(),
+		password: new FormControl(),
+		email: new FormControl("", [Validators.required, Validators.email]),
+		name: new FormControl(),
+		lastname: new FormControl(),
+	});
+
+	onSubmit(): void {
+		let authFlow = this.loginService
+			.register(this.registerGroup.value)
+			.pipe(switchMap(() => this.loginService.profile()));
+
+		authFlow.subscribe({
+			next: (user: User) => {
+				this.loginService.saveUserToLocalStorage(user);
+				this.dialog.closeAll();
+			},
+			error: (error) => {
+				this.isUserExist = true;
+				setTimeout(() => {
+					this.isUserExist = false;
+				}, 3000);
+			},
+		});
+	}
 }
--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,13 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
+import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
+import {response} from "express";
+import {lastValueFrom, switchMap} from "rxjs";
+import {User} from "src/app/model/user";
+import {LoginService} from "src/app/shared/login.service";
+import {UserService} from "src/app/shared/user.service";
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css'],
+	selector: "app-profile",
+	templateUrl: "./profile.component.html",
+	styleUrls: ["./profile.component.css"],
 })
 export class ProfileComponent implements OnInit {
-  constructor() {}
+	public user?: User;
 
-  ngOnInit(): void {}
-  hide = true;
+	constructor(
+		private userService: UserService,
+		private loginService: LoginService,
+		private formBuilder: FormBuilder
+	) {}
+	saveGroup = new FormGroup({
+		username: new FormControl(),
+		password: new FormControl(),
+		email: new FormControl(),
+		name: new FormControl(),
+		lastname: new FormControl(),
+		photo: new FormControl(),
+		role: new FormControl(),
+		gold: new FormControl(),
+		silver: new FormControl(),
+		bronze: new FormControl(),
+		numberOfGames: new FormControl(),
+		numberOfWins: new FormControl(),
+		id: new FormControl(),
+	});
+
+	async ngOnInit() {
+		const users$ = this.userService.getUser();
+		this.user = await lastValueFrom(users$);
+		this.saveGroup.setValue({
+			username: this.user.username,
+			password: null,
+			email: this.user.email,
+			name: this.user.name,
+			lastname: this.user.lastname,
+			photo: null,
+			role: this.user.role,
+			gold: this.user.gold,
+			silver: this.user.silver,
+			bronze: this.user.bronze,
+			numberOfGames: this.user.numberOfGames,
+			numberOfWins: this.user.numberOfWins,
+			id: this.user.id,
+		});
+	}
+	onSubmit(): void {
+		let authFlow = this.userService
+			.updateUser(this.saveGroup.value)
+			.pipe(switchMap(() => this.loginService.profile()));
+
+		authFlow.subscribe({
+			next: (user: User) => {
+				this.loginService.saveUserToLocalStorage(user);
+			},
+			error: (error) => {},
+		});
+	}
+	hide = true;
 }
