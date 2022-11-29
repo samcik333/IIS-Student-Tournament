8e5f9c0df09900a195af4e4edf528c162c610722
Samuel Sadlek <samcik333@gmail.com>
2022-11-29 06:02:42 +0100

    push final

--- src/app/tournament/info/info.component.ts
+++ src/app/tournament/info/info.component.ts
@@ -1,69 +1,71 @@
-import { Component, OnInit } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { ActivatedRoute, Params } from '@angular/router';
-import { lastValueFrom } from 'rxjs';
-import { Team } from 'src/app/model/team';
-import { Tournament } from 'src/app/model/tournament';
-import { TeamService } from 'src/app/shared/team.service';
-import { TournamentService } from 'src/app/shared/tournament.service';
+import {Component, OnInit} from "@angular/core";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {ActivatedRoute, Params} from "@angular/router";
+import {lastValueFrom} from "rxjs";
+import {Team} from "src/app/model/team";
+import {Tournament} from "src/app/model/tournament";
+import {TeamService} from "src/app/shared/team.service";
+import {TournamentService} from "src/app/shared/tournament.service";
 
 @Component({
-  selector: 'app-info',
-  templateUrl: './info.component.html',
-  styleUrls: ['./info.component.css'],
+	selector: "app-info",
+	templateUrl: "./info.component.html",
+	styleUrls: ["./info.component.css"],
 })
 export class InfoComponent implements OnInit {
-  myParam!: string;
-  tournament!: Tournament;
-  teamList: Array<Team> = [];
+	myParam!: string;
+	tournament!: Tournament;
+	teamList: Array<Team> = [];
 
-  constructor(
-    private route: ActivatedRoute,
-    private routerTournament: TournamentService,
-    private teamService: TeamService,
-    private snackBar: MatSnackBar
-  ) {}
+	constructor(
+		private route: ActivatedRoute,
+		private routerTournament: TournamentService,
+		private teamService: TeamService,
+		private snackBar: MatSnackBar
+	) {}
 
-  async ngOnInit() {
-    this.route.params.subscribe(
-      (params: Params) => (this.myParam = params['id'])
-    );
-    this.routerTournament
-      .find(this.myParam)
-      .subscribe((response: Tournament) => {
-        this.tournament = response;
-      });
+	async ngOnInit() {
+		this.route.params.subscribe(
+			(params: Params) => (this.myParam = params["id"])
+		);
+		this.routerTournament
+			.find(this.myParam)
+			.subscribe((response: Tournament) => {
+				this.tournament = response;
+			});
 
-    const teams$ = this.teamService.getOwnedTeams();
-    this.teamList = await lastValueFrom(teams$);
-  }
+		const teams$ = this.teamService.getOwnedTeams();
+		this.teamList = await lastValueFrom(teams$);
+	}
 
-  openSnackBar(errMessage: string) {
-    this.snackBar.open(errMessage, '', { duration: 2500 });
-  }
+	openSnackBar(errMessage: string) {
+		this.snackBar.open(errMessage, "", {duration: 2500});
+	}
 
-  join(tournamentId: number, id: string) {
-    if (id == 'user') {
-      id = localStorage.getItem('userID') || '';
-      this.routerTournament.addPlayer(tournamentId).subscribe({
-        next: (res) => {
-          console.log(res);
-        },
-        error: (e) => {
-          console.log(e);
-          this.openSnackBar(e.error.message);
-        },
-      });
-    } else {
-      this.routerTournament.addTeam(tournamentId, id).subscribe({
-        next: (res) => {
-          console.log(res);
-        },
-        error: (e) => {
-          console.log(e);
-          this.openSnackBar(e.error.message);
-        },
-      });
-    }
-  }
+	join(tournamentId: number, id: string) {
+		if (id == "user") {
+			id = localStorage.getItem("userID") || "";
+			this.routerTournament.addPlayer(tournamentId).subscribe({
+				next: (res) => {
+					console.log(res);
+					this.ngOnInit();
+				},
+				error: (e) => {
+					console.log(e);
+					this.openSnackBar(e.error.message);
+				},
+			});
+		} else {
+			this.routerTournament.addTeam(tournamentId, id).subscribe({
+				next: (res) => {
+					console.log(res);
+					this.ngOnInit();
+				},
+				error: (e) => {
+					console.log(e);
+					this.openSnackBar(e.error.message);
+				},
+			});
+		}
+	}
 }
