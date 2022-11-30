cc996a4ec5fe806700cf5ddbc7046aaee97ee487
Samuel Sadlek <samcik333@gmail.com>
2022-11-30 22:23:31 +0100

    service repair

--- src/app/shared/team.service.ts
+++ src/app/shared/team.service.ts
@@ -6,14 +6,14 @@ import {Team} from "../model/team";
 import {User} from "../model/user";
 import {environment} from "src/environments/environment";
 
-const endpoint = `http://localhost:5005/`;
+const endpoint = `https://sjs-squad.herokuapp.com/`;
 
 const httpOptions = {
 	headers: new HttpHeaders({
-	  'Content-Type': 'application/json',
-	  'Access-Control-Allow-Headers': 'x-access-token'
-	})
-  };
+		"Content-Type": "application/json",
+		"Access-Control-Allow-Headers": "x-access-token",
+	}),
+};
 
 @Injectable({
 	providedIn: "root",
@@ -23,61 +23,76 @@ export class TeamService {
 
 	getTeams(): Observable<Team[]> {
 		return this.http.get<Team[]>(endpoint + `teams`, {
-			withCredentials: true, headers:httpOptions.headers
+			withCredentials: true,
+			headers: httpOptions.headers,
 		});
 	}
 
 	getOwnedTeams(): Observable<Team[]> {
 		return this.http.get<Team[]>(endpoint + `ownedTeams`, {
-			withCredentials: true, headers:httpOptions.headers
+			withCredentials: true,
+			headers: httpOptions.headers,
 		});
 	}
 
 	getPlayers(id: string): Observable<User[]> {
-		return this.http.get<User[]>(endpoint + `teamPlayerList/` + id, {headers:httpOptions.headers});
+		return this.http.get<User[]>(endpoint + `teamPlayerList/` + id, {
+			headers: httpOptions.headers,
+		});
 	}
 
 	getOwner(id: string): Observable<User> {
-		return this.http.get<User>(endpoint + `teamOwner/` + id, {headers:httpOptions.headers});
+		return this.http.get<User>(endpoint + `teamOwner/` + id, {
+			headers: httpOptions.headers,
+		});
 	}
 
 	addPlayer(id: string, data: any): Observable<any> {
 		return this.http.post(endpoint + `teamAddPlayer/` + id, data, {
-			withCredentials: true, headers:httpOptions.headers
+			withCredentials: true,
+			headers: httpOptions.headers,
 		});
 	}
 
 	updateTeam(id: string, data: any): Observable<any> {
 		return this.http.put(endpoint + `team/` + id, data, {
-			withCredentials: true, headers:httpOptions.headers
+			withCredentials: true,
+			headers: httpOptions.headers,
 		});
 	}
 
 	deletePlayer(id: string, username: string): Observable<any> {
 		return this.http.delete(endpoint + "teamDelPlayer/" + id, {
 			body: {username},
-			withCredentials: true, headers:httpOptions.headers
+			withCredentials: true,
+			headers: httpOptions.headers,
 		});
 	}
 
 	create(data: any): Observable<any> {
 		return this.http.post(endpoint + `teams`, data, {
-			withCredentials: true, headers:httpOptions.headers
+			withCredentials: true,
+			headers: httpOptions.headers,
 		});
 	}
 
 	findTeam(id: string): Observable<Team> {
-		return this.http.get<Team>(endpoint + "team/" + id, {headers:httpOptions.headers});
+		return this.http.get<Team>(endpoint + "team/" + id, {
+			headers: httpOptions.headers,
+		});
 	}
 
 	findUser(id: string): Observable<User> {
-		return this.http.get<User>(endpoint + "teamPlayer/" + id, {headers:httpOptions.headers});
+		return this.http.get<User>(endpoint + "teamPlayer/" + id, {
+			headers: httpOptions.headers,
+		});
 	}
 
 	delete(name: string): Observable<any> {
 		return this.http.delete(endpoint + "teams", {
 			body: {name},
-			withCredentials: true, headers:httpOptions.headers
+			withCredentials: true,
+			headers: httpOptions.headers,
 		});
 	}
 }
